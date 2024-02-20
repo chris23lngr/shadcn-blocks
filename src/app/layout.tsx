a21077577a9c9a8ac92c94e1669d4714ab006f29
Christoph Langer <christoph.langer100@gmail.com>
2024-02-20 14:36:28 +0100

    fix(styles): move globals.css to @/styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import { Navbar } from '@/components/navbar';
 import { Toaster } from '@/components/ui/sonner';
+import '@/styles/globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
